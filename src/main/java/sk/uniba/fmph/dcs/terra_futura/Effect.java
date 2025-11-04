a29ae3b28377ceb25ff9fc685822fdfbcb3a7387
martyniyk650-svg <martyniyk650@gmail.com>
2025-11-04 19:16:39 +0200

    Added interface "Effect"

---
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Effect.java
@@ -0,0 +1,9 @@
+package sk.uniba.fmph.dcs.terra_futura;
+
+import java.util.*;
+
+public interface Effect {
+    boolean check(List<Resource> input, List<Resource> output, int pollution);
+    boolean hasAssistance();
+    String state();
+}
