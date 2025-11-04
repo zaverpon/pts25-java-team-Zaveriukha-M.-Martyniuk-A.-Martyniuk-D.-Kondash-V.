20ef1ce32c54a177595834e8e67fff930c29a32e
martyniyk650-svg <martyniyk650@gmail.com>
2025-11-04 18:28:20 +0200

    testа

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/Effect.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Effect.java
@@ -2,6 +2,6 @@ package sk.uniba.fmph.dcs.terra_futura;
 
 public interface Effect {
     public static void main(String[] args) {
-        System.out.println("Hello Wosld");
+        System.out.println("Hello Wosd");
     }
 }
