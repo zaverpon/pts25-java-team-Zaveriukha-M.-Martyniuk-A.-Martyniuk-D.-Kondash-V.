24193c8abb15d4884d7d8bb3ee7b84a2b40714d9
ronaldo <martuniykden2007@gmail.com>
2025-11-12 13:47:59 +0100

    added a class SelectReward

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/SelectReward.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/SelectReward.java
@@ -1,9 +1,68 @@
 package sk.uniba.fmph.dcs.terra_futura;
 
+import org.json.*;
+import java.util.*;
+
 public class SelectReward {
-    public static void main(String[] args) {
-        System.out.println("Everything working");
-        System.out.println("Vitalik ahoj koo");
+    private Optional<Integer> player;
+    private List<Resource> selelction;
+
+    private Card targetCard;
+
+    public SelectReward(){
+        this.player = Optional.empty();
+        this.selelction = Collections.emptyList();
+        this.targetCard = null;
+    }
+
+    public void setReward(int player, Card card, Resource[] reward){
+        if(this.player.isPresent()){
+            throw new IllegalArgumentException("Reward selelction in progress");
+        }
+        if(card == null){
+            throw new IllegalArgumentException("card is null");
+        }
+        if(reward == null){
+            throw new IllegalArgumentException("reward is null");
+        }
+        this.player = Optional.of(player);
+        this.targetCard = card;
+        this.selelction = new ArrayList<>(Arrays.asList(reward));
+    }
+
+    public boolean canSelectReward(Resource resource){
+        if(resource == null || targetCard == null || player.isEmpty()){
+            return false;
+        }
+        if (!this.selelction.contains(resource)){
+            return false;
+        }
+
+        return targetCard.canGetResources(List.of(resource));
+    }
+
+    public void selectReward(Resource resource){
+        if(!canSelectReward(resource)){
+            throw new IllegalArgumentException("Cannot select the given resource");
+        }
+
+        targetCard.getResources(List.of(resource));
+
+        this.player = Optional.empty();
+        this.selelction = Collections.emptyList();
+        this.targetCard = null;
+    }
+
+    public String state(){
+        JSONObject obj = new JSONObject();
+        obj.put("player", this.player.isPresent() ? this.player.get() : JSONObject.NULL);
+
+        JSONArray arr = new JSONArray();
+        for (Resource r : this.selelction) {
+            arr.put(r.name());
+        }
+        obj.put("selection", arr);
+        return obj.toString();
     }
 
 }
