c2cdbe29d3a4c719a861b94552539d29c6d47ea0
zkk1p0@gmail.com <vitaliykondash@gmail.com>
2025-11-14 16:19:41 +0100

    add Pile class

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/Pile.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Pile.java
@@ -1,4 +1,55 @@
 package sk.uniba.fmph.dcs.terra_futura;
 
-public class Pile {
+import java.util.*;
+
+public class Pile  {
+    private List<Card> visibleCards;
+    private Deque<Card> hiddenCards;
+
+    public Pile(List<Card> visible)  {
+        if (visible.size() > 4)  {
+            throw new IllegalArgumentException("visibleCards must be <= 4");
+        }
+        this.visibleCards = new ArrayList<>(visible);
+        this.hiddenCards = new ArrayDeque<>();
+    }
+
+    public Pile(List<Card> visible , List<Card> hidden)  {
+        if (visible.size() > 4)  {
+            throw new IllegalArgumentException("visibleCards must be <= 4");
+        }
+        this.visibleCards = new ArrayList<>(visible);
+        this.hiddenCards = new ArrayDeque<>(hidden);
+    }
+
+    public Optional<Card> getCard(int index)  {
+        if (index >= 0 && index < visibleCards.size())  {
+            return Optional.of(visibleCards.get(index));
+        }  else {
+            return Optional.empty();
+        }
+    }
+
+    public void takeCard(int index)  {
+        if (index < 0 || index >= visibleCards.size()) {
+            throw new IllegalArgumentException("illegal index");
+        }
+            visibleCards.remove(index);
+            if (!hiddenCards.isEmpty()) {
+                visibleCards.add(index, hiddenCards.removeLast());
+
+        }
+    }
+
+    public void removeLastCard()  {
+        if (visibleCards.isEmpty()) return;
+        visibleCards.remove(0);
+        if (!hiddenCards.isEmpty())  {
+            visibleCards.add(hiddenCards.removeLast());
+        }
+    }
+
+    public String state()  {
+        return "Pile: visible - " + visibleCards.size() + ", hidden - " + hiddenCards.size();
+    }
 }
