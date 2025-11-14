cf92cb46efbd14adac719dd95b2433f7e5a690c1
zkk1p0@gmail.com <vitaliykondash@gmail.com>
2025-11-14 20:20:07 +0100

    Revert "add <datatype> Points"
    This reverts commit bf90b23216afe30b8593a74283d57fbdc81288f1.

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/Points.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Points.java
@@ -1,31 +1,4 @@
 package sk.uniba.fmph.dcs.terra_futura;
 
-public class Points implements Comparable<Points> {
-    private final int val;
-
-    public Points(int value)  {
-        this.val = value;
-    }
-
-    public int getValue()  {
-        return val;
-    }
-
-    public Points plus(Points other)  {
-        return new Points(val + other.val);
-    }
-    public Points minus(Points other)  {
-        return new Points(val - other.val);
-    }
-
-    @Override
-    public int compareTo(Points p)  {
-        return Integer.compare(this.val, p.getValue());
-    }
-
-    @Override
-    public String toString()  {
-        return "Points:" + val;
-    }
-
+public class Points {
 }
