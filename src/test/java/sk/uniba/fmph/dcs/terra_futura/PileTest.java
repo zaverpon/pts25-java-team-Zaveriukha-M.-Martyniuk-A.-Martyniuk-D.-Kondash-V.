8f61a2b03c4a25181db00cfde77c382bd967f0a0
zkk1p0@gmail.com <vitaliykondash@gmail.com>
2025-11-19 14:13:29 +0100

    add some tests for hidden cards

--- src/test/java/sk/uniba/fmph/dcs/terra_futura/PileTest.java
+++ src/test/java/sk/uniba/fmph/dcs/terra_futura/PileTest.java
@@ -14,14 +14,16 @@ import static org.junit.Assert.*;
 public class PileTest {
 
     private Pile pile;
-    private Card card1, card2;
+    private Card vis1, vis2, hid1, hid2;
 
     @Before
     public void setUp()  {
-        card1 = new Card(new Resource[0], 0);
-        card2 = new Card(new Resource[0], 1);
+        vis1 = new Card(new Resource[0], 0);
+        vis2 = new Card(new Resource[0], 1);
+        hid1 = new Card(new Resource[0], 0);
+        hid2 = new Card(new Resource[0], 1);
 
-        pile =  new Pile(List.of(card1, card2));
+        pile =  new Pile(List.of(vis1, vis2), List.of(hid1, hid2));
     }
 
     /*checks if the State string is null*/
@@ -34,13 +36,15 @@ public class PileTest {
     public void takeCardTr()  {
         Optional<Card> c = pile.getCard(0);
         assertTrue(c.isPresent());
-        assertEquals(card1, c.get());
+        assertEquals(vis1, c.get());
 
         pile.takeCard(0);
 
         assertTrue(pile.getCard(0).isPresent());
-        assertEquals(card2, pile.getCard(0).get());
-        assertFalse(pile.getCard(1).isPresent());
+        assertSame(hid2, pile.getCard(0).get());
+        assertTrue(pile.getCard(1).isPresent());
+        assertSame(vis2, pile.getCard(1).get());
+        assertFalse(pile.getCard(2).isPresent());
 
     }
 
@@ -57,12 +61,50 @@ public class PileTest {
         assertFalse(pile.getCard(5).isPresent());
     }
 
+    /*Checks whether removeLastCard works correctly when there are no hidden cards.*/
     @Test
     public void remLastCard()  {
         pile.removeLastCard();
-        pile.removeLastCard();
+
+        assertSame(vis2, pile.getCard(0).get());
+        assertSame(hid2, pile.getCard(1).get());
+        assertFalse(pile.getCard(2).isPresent());
 
         pile.removeLastCard();
-        assertFalse(pile.getCard(0).isPresent());
+
+        assertSame(hid2, pile.getCard(0).get());
+        assertSame(hid1, pile.getCard(1).get());
+        assertFalse(pile.getCard(2).isPresent());
+
+        pile.removeLastCard();
+
+        assertTrue(pile.getCard(0).isPresent());
+        assertSame(hid1, pile.getCard(0).get());
+        assertFalse(pile.getCard(1).isPresent());
     }
+
+    /*checks whether the hidden card falls exactly on the index
+     of the visible card that was taken*/
+    @Test
+    public void sameIndex()  {
+        pile.takeCard(1);
+
+        assertTrue(pile.getCard(1).isPresent());
+        assertSame(hid2, pile.getCard(1).get());
+
+        assertSame(vis1, pile.getCard(0).get());
+        assertFalse(pile.getCard(2).isPresent());
+    }
+
+    /*Checks whether removeLastCard works correctly in the presence of hidden cards.*/
+    @Test
+    public void remLastCardWithHidd()  {
+        pile.removeLastCard();
+
+        assertSame(vis2, pile.getCard(0).get());
+        assertSame(hid2, pile.getCard(1).get());
+        assertFalse(pile.getCard(2).isPresent());
+    }
+
+
 }
